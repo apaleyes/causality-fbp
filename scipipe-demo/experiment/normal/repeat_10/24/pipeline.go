5765c4eb1087e1ac6344a2a674cd305591b1547e
Andrei Paleyes <apalees@gmail.com>
2023-03-13 02:57:27 +0000

    complete overhaul of the experiment

--- scipipe-demo/experiment/normal/repeat_10/24/pipeline.go
+++ scipipe-demo/experiment/normal/repeat_10/24/pipeline.go
@@ -1,9 +1,14 @@
+// calculation of GC ratio across two substrings from DNA samples
+// inspired by https://github.com/scipipe/scipipe/tree/master/examples/scatter_gather
+
 package main
 
 import (
 	"fmt"
 	"math/rand"
     "time"
+	"os"
+	"path"
 	. "github.com/scipipe/scipipe"
 	comp "github.com/scipipe/scipipe/components"
 )
@@ -15,51 +20,74 @@ func get_random_int(min, max int) int {
 }
 
 func main() {
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		fmt.Println("Usage: ")
+		fmt.Println("    pipeline.go <abs path to DNA segment files> <mode: normal|break-count1|break-count2>")
+		fmt.Println("")
+		fmt.Println("mode is optional, normal is used by default")
+		os.Exit(1)
+	}
+
+	base_dir := os.Args[1]
+	mode := "normal"
+	if len(os.Args) == 3 {
+		mode = os.Args[2]
+	}
+
 	wf := NewWorkflow("gc_ratio_wf", 4)
 
-	// normal
-	count := get_random_int(50, 100)
-	// break count
-	// count := get_random_int(10, 20)
+	var count1 int
+	if mode == "break-count1" {
+		// this range is too small and thus gives lots of variability
+		count1 = get_random_int(1, 10)
+	} else {
+		// normal
+		count1 = get_random_int(1, 100)
+	}
+	
+	var count2 int
+	if mode == "break-count2" {
+		// this range is too small and thus gives lots of variability
+		count2 = get_random_int(1, 10)
+	} else {
+		// normal
+		count2 = get_random_int(1, 100)
+	}
 
-	// normal
-	start := get_random_int(90, 110)
-	// break start
-	// start := get_random_int(180, 190)
+	file_1 := path.Join(base_dir, "dna_segment_1")
+	file_2 := path.Join(base_dir, "dna_segment_2")
 
-	base_dir := "/home/ubuntu/tmp/y_fasta/"
-	file_1 := base_dir + "dna_segment_1"
-	file_2 := base_dir + "dna_segment_2"
+	writeNumberCommand := "echo %d > {o:count}"
+	count1Proc := wf.NewProc("count1", fmt.Sprintf(writeNumberCommand, count1))
+	count1Proc.SetOut("count", "count1.txt")
 
-	writeNumberCommand := "echo %d > {o:%s}"
-	countProc := wf.NewProc("count", fmt.Sprintf(writeNumberCommand, count, "count"))
-	countProc.SetOut("count", "count.txt")
+	count2Proc := wf.NewProc("count2", fmt.Sprintf(writeNumberCommand, count2))
+	count2Proc.SetOut("count", "count2.txt")
 
-	startProc := wf.NewProc("start", fmt.Sprintf(writeNumberCommand, start, "start"))
-	startProc.SetOut("start", "start.txt")
+	// start1 := 1
+	// start2 := 190
 
-
-	charCountCommand := "sed -n $(cat {i:start}),$(($(cat {i:start}) + $(cat {i:count})))p %s | fold -w 1 | grep '[%s]' | wc -l | awk '{ print $1 }' > {o:%s}"
+	charCountCommand := "sed -n 1,$(cat {i:count})p %s | fold -w 1 | grep '[%s]' | wc -l | awk '{ print $1 }' > {o:%s}"
 
 	gccnt1 := wf.NewProc("gccount1", fmt.Sprintf(charCountCommand, file_1, "GC", "gccount"))
 	gccnt1.SetOut("gccount", "chry.fa.gccnt1")
-	gccnt1.In("start").From(startProc.Out("start"))
-	gccnt1.In("count").From(countProc.Out("count"))
+	// gccnt1.In("start").From(startProc.Out("start"))
+	gccnt1.In("count").From(count1Proc.Out("count"))
 
 	gccnt2 := wf.NewProc("gccount2", fmt.Sprintf(charCountCommand, file_2, "GC", "gccount"))
 	gccnt2.SetOut("gccount", "chry.fa.gccnt2")
-	gccnt2.In("start").From(startProc.Out("start"))
-	gccnt2.In("count").From(countProc.Out("count"))
+	// gccnt2.In("start").From(startProc.Out("start"))
+	gccnt2.In("count").From(count2Proc.Out("count"))
 
 	atcnt1 := wf.NewProc("atcount1", fmt.Sprintf(charCountCommand, file_1, "AT", "atcount"))
 	atcnt1.SetOut("atcount", "chry.fa.atcnt1")
-	atcnt1.In("start").From(startProc.Out("start"))
-	atcnt1.In("count").From(countProc.Out("count"))
+	// atcnt1.In("start").From(startProc.Out("start"))
+	atcnt1.In("count").From(count1Proc.Out("count"))
 
 	atcnt2 := wf.NewProc("atcount2", fmt.Sprintf(charCountCommand, file_2, "AT", "atcount"))
 	atcnt2.SetOut("atcount", "chry.fa.atcnt2")
-	atcnt2.In("start").From(startProc.Out("start"))
-	atcnt2.In("count").From(countProc.Out("count"))
+	// atcnt2.In("start").From(startProc.Out("start"))
+	atcnt2.In("count").From(count2Proc.Out("count"))
 
 	// Concatenate GC & AT counts
 	gccat := comp.NewConcatenator(wf, "gccat", "gccounts.txt")
@@ -85,6 +113,19 @@ func main() {
 	gcrat.SetOut("gcratio", "gcratio.txt")
 	gcrat.In("gcsum").From(gcsum.Out("sum"))
 	gcrat.In("atsum").From(atsum.Out("sum"))
+
+	// if graph of the workflow is desired
+	// uncomment line below
+	// one caveat is that by default, if you just call PlotGraphDPF,
+	// this produces graph with all ports named
+	// and that looks messy
+	// so as a workaround one can produce dot file
+	// then edit it manually to get rid of port names
+	// and then covert it to PDF with
+	//   dot -Tpdf workflow_graph.dot -o workflow_graph.dot.pdf
+	// also note this required graphviz installed on our system
+        
+	wf.PlotGraph("workflow_graph.dot")
 
 	// === RUN PIPELINE ===========================================================================
 
